53b752f5601318e02260b9f019ebd69add4bc17a
Hannah <hannahdubusker@gmail.com>
2023-12-25 15:30:21 -0600

    Let's only register global commands if they don't already exist

--- ItsAnotherBot/Program.cs
+++ ItsAnotherBot/Program.cs
@@ -22,16 +22,28 @@ public class Program
 
     public async Task ClientReady()
     {
-        // Command already registered
-        //var globalCommand = new SlashCommandBuilder();
-        //globalCommand.WithName("test-command");
-        //globalCommand.WithDescription("a test global command for testing purposes");
-        //await _client.CreateGlobalApplicationCommandAsync(globalCommand.Build());
 
-        //var globalCommand2 = new SlashCommandBuilder();
-        //globalCommand2.WithName("toast");
-        //globalCommand2.WithDescription("if you can do this, the deployment pipeline worked");
-        //await _client.CreateGlobalApplicationCommandAsync(globalCommand2.Build());
+        var existingCommands = await _client.GetGlobalApplicationCommandsAsync();
+        List<SlashCommandBuilder> commands = new List<SlashCommandBuilder>() { };
+
+        var globalCommand = new SlashCommandBuilder();
+        globalCommand.WithName("test-command");
+        globalCommand.WithDescription("a test global command for testing purposes");
+        commands.Add(globalCommand);
+
+        var globalCommand2 = new SlashCommandBuilder();
+        globalCommand2.WithName("toast");
+        globalCommand2.WithDescription("if you can do this, the deployment pipeline worked");
+        commands.Add(globalCommand2);
+
+
+        foreach (SlashCommandBuilder newCommand in commands)
+        {
+            if (!commands.Exists(cmd => cmd.Name == newCommand.Name))
+            {
+                await _client.CreateGlobalApplicationCommandAsync(newCommand.Build());
+            }
+        }
     }
 
     private async Task SlashCommandHandler(SocketSlashCommand command)
