83982422050af25d5ce01ebd1a2660a2ce0646c0
Hannah <hannahdubusker@gmail.com>
2023-12-24 20:41:10 -0600

    test infrastructure

---
+++ ItsAnotherBot/CommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItsAnotherBot
+{
+    public class CommandHandler
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly CommandService _commands;
+
+        public CommandHandler(DiscordSocketClient client, CommandService commands)
+        {
+            _commands = commands;
+            _client = client;
+        }
+
+        public async Task InstallCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+            await _commands.AddModulesAsync(assembly: null, services: null);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage messageParam)
+        {
+            var message = messageParam as SocketUserMessage;
+            if (message == null)
+            {
+                return;
+            }
+
+
+        }
+    }
+}
--- ItsAnotherBot/Program.cs
+++ ItsAnotherBot/Program.cs
@@ -1,9 +1,35 @@
-﻿public class Program
+﻿using Discord;
+using Discord.WebSocket;
+
+public class Program
 {
+    private DiscordSocketClient _client;
     public static Task Main(string[] args) => new Program().MainAsync();
 
     public async Task MainAsync()
     {
+        _client = new DiscordSocketClient();
+        var token = File.ReadAllText("token.txt");
 
+        _client.Ready += ClientReady;
+        _client.SlashCommandExecuted += SlashCommandHandler;
+
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+
+        await Task.Delay(-1);
+    }
+
+    public async Task ClientReady()
+    {
+        var globalCommand = new SlashCommandBuilder();
+        globalCommand.WithName("test-command");
+        globalCommand.WithDescription("a test global command for testing purposes");
+        await _client.CreateGlobalApplicationCommandAsync(globalCommand.Build());
+    }
+
+    private async Task SlashCommandHandler(SocketSlashCommand command)
+    {
+        await command.RespondAsync("You done did the testing command.");
     }
 }
